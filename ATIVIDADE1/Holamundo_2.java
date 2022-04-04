225f17ff8a8bf6e96cbfd7bbc9dadbd22e61d5c6
GabrielFNP <102600004+GabrielFNP@users.noreply.github.com>
2022-04-04 17:05:17 -0300

    Add files via upload

---
+++ ATIVIDADE1/Holamundo_2.java
@@ -0,0 +1,14 @@
+package ATIVIDADE1;
+
+public class Holamundo_2 {
+    public static void main(String[] args){
+        Olamundo objeto1 = new Olamundo();
+        Olamundo objeto2 = new Olamundo();
+
+        objeto2.Turma= 'c';
+        System.out.println("Ola mundo, turma"+ objeto1+"\n");
+        System.out.println("Nova Turma adicionada"+ objeto2.turma);
+
+    }
+    
+}
