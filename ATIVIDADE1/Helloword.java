4b237d57060f652db0d04a7c9884df56be98eecb
GabrielFNP <102600004+GabrielFNP@users.noreply.github.com>
2022-04-04 16:51:13 -0300

    Add files via upload

---
+++ ATIVIDADE1/Helloword.java
@@ -0,0 +1,10 @@
+package ATIVIDADE1;
+
+public class Helloword {
+    public static void main(String[] args){
+
+
+    System.out.println("\n");
+    System.out.println("wello word");
+    System.out.println("\n");
+}}
