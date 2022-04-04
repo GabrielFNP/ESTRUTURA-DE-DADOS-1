99f864b9217373e693ecc47569de7f8d05d990ed
GabrielFNP <102600004+GabrielFNP@users.noreply.github.com>
2022-04-04 20:22:23 -0300

    Update Holamundo_2.java

--- ATIVIDADE1/Holamundo_2.java
+++ ATIVIDADE1/Holamundo_2.java
@@ -1,14 +1,13 @@
-package ATIVIDADE1;
+package Aula02;
 
-public class Holamundo_2 {
+public class Olamundo {
+    private char Turma;
+
     public static void main(String[] args){
         Olamundo objeto1 = new Olamundo();
         Olamundo objeto2 = new Olamundo();
 
         objeto2.Turma= 'c';
-        System.out.println("Ola mundo, turma"+ objeto1+"\n");
-        System.out.println("Nova Turma adicionada"+ objeto2.turma);
-
-    }
-    
-}
+        System.out.println("Ola mundo, turma"+ objeto1.Turma);
+        System.out.println("Nova Turma adicionada"+ "\n"+objeto2.Turma);
+}}
